0206b9bd4454804ad3d8fb77006accf0b7a09229
abid kasu <abidkasu@gmail.com>
2025-03-02 00:10:01 +0530

    changed some packages

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -4,6 +4,8 @@ import { Box, Button, Card, Flex, Grid, Heading, Text } from "@radix-ui/themes";
 import delay from "delay";
 import { notFound } from "next/navigation";
 import ReactMarkdown from "react-markdown";
+import { Pencil2Icon } from "@radix-ui/react-icons";
+import Link from "next/link";
 
 interface IssueDetailPageProps {
   params: {
@@ -20,19 +22,28 @@ const IssueDetailPage = async ({ params }: IssueDetailPageProps) => {
   }
   await delay(2000);
   return (
-    <Grid columns={"2"} gap="4">
+    <Grid
+      columns={{
+        initial: "1",
+        md: "2",
+      }}
+      gap="4"
+    >
       <Box>
         <Heading>{issue.title}</Heading>
         <Flex className="space-x-3 items-center" my={"2"}>
           <IssueStatus status={issue.status} />
           <Text>{issue.createdAt.toDateString()}</Text>
         </Flex>
-        <Card>
+        <Card className="prose" mt="4">
           <ReactMarkdown>{issue.description}</ReactMarkdown>
         </Card>
       </Box>
       <Box>
-        <Button>Close Issue</Button>
+        <Button>
+          <Pencil2Icon />
+          <Link href={`/issues/${issue.id}/edit`}>Edit</Link>
+        </Button>
       </Box>
     </Grid>
   );
