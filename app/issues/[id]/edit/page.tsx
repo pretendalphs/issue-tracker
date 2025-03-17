62b487dab7704e9019fe22a8bdb6afb748960982
pretendalphs <abidkasu@gmail.com>
2025-03-17 23:00:38 +0530

    sameed-init-2

--- app/issues/[id]/edit/page.tsx
+++ app/issues/[id]/edit/page.tsx
@@ -3,11 +3,11 @@ import prisma from "@/prisma/client";
 import { notFound } from "next/navigation";
 import IssueFormWrapper from "../../_components/IssueFormWrapper";
 
-interface Props {
+interface InterfaceProps {
   params: { id: string };
 }
 
-const EditIssuePage = async ({ params }: Props) => {
+const EditIssuePage = async ({ params }: InterfaceProps) => {
   const id = await params.id;
 
   const issue = await prisma.issue.findUnique({
